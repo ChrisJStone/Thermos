3e474482f711db6248f92b19b3c3cb16dbb85463
Terminal-Access <Preatorian20!&>
2018-10-15 01:02:35 +0100

    Rebranding back to Thermos Post Fork URL Update's adjusting build settings and Travis CI settings. Moved bukkit submodule back to proper commit

--- travii/make.sh
+++ travii/make.sh
@@ -1,15 +1,24 @@
 mkdir $HOME/release
 rm -r $HOME/release/*
 
-# 2422
-cd forge
-git checkout 0965ebae0942421628810aff1a5360b4ae8715b9
+# 2185
+cd MinecraftForge
+git checkout 7065cb36bf8f1f7a5ad14c0abc2c72e5e5d731ed
 cd ..
-git pull origin master
+git pull origin 1.10.x
 ./gradlew --parallel -PofficialBuild clean setupCauldron --parallel jar packageBundle
-cp build/distributions/Contigo*server* $HOME/release/Contigo-1.10.2-2422-server.jar
+cp build/distributions/Thermos*server* $HOME/release/Thermos-1.10.2-2185-server.jar
+
+# 2511
+cd MinecraftForge
+git checkout e4caf46cb954ea86fe338cd256402cd84f824586
+cd ..
+git pull origin 1.10.x
+./gradlew --parallel -PofficialBuild clean setupCauldron --parallel jar packageBundle
+cp build/distributions/Thermos*server* $HOME/release/Thermos-1.10.2-2511-server.jar
+
 cd build/distributions/
-unzip Contigo*bundle*zip
+unzip Thermos*bundle*zip
 rm -r bin/unknown
 mv bin libraries
 zip -r libraries.zip libraries
